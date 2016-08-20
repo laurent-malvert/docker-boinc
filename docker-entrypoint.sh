10f9092c2e9c44f31edc67076ccc7df8eab004a2
Rowan <admin@rwky.net>
2016-08-20 11:37:38 +0100

    /dev/input/mice error workaround and documentation changes (#2)
    * Updated README
    
    * Added documentation for persistent configuration
    * Added documentation for running virtualbox
    
    * Added work around for unable to open /dev/input/mice error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,8 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 if [ "$1" = 'boinc' ]; then
+    #work around for activity checking which sends errors to stderr if this directory doesn't exist
+    mkdir -p /dev/input/mice
     #fix permissions
     chown -R boinc:boinc /var/lib/boinc-client
     #reconfigure boinc just in case /var/lib/boinc-client is a volume and hasn't been used before
